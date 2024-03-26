fb74ec27223bd0d592c5169f609a4770c8add955
CnaMokhtari <mohammad.mokhtari@univ-tlse3.fr>
2024-03-26 19:22:03 +0100

    créationdelaclasseRonin

---
+++ src/histoire/HistoireTP4.java
@@ -0,0 +1,43 @@
+package histoire;
+
+import personnages.Humain;
+import personnages.Commercant;
+import personnages.Yakuza;
+import personnages.Ronin;
+
+public class HistoireTP4 {
+ /*
+    public static void main(String[] args) {
+        Humain prof = new Humain("Prof", "kombucha", 54);
+        prof.direBonjour();
+        prof.acheter("boisson", 12);
+        prof.boire();
+        prof.acheter("jeu", 2);
+        prof.acheter("kimono", 50);
+    }*/
+ /*public static void main(String[] args) {
+        Commercant marco = new Commercant("Marco", 20);
+        marco.direBonjour();
+        marco.seFaireExtorquer();
+        marco.recevoir(15);
+        marco.boire();
+    }*/
+ /*public static void main(String[] args) {
+        Commercant marco = new Commercant("Marco", 20);
+        Yakuza yakuLeNoir = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
+
+        yakuLeNoir.direBonjour();
+        yakuLeNoir.extorquer(marco);
+    }*/
+ public static void main(String[] args) {
+        Commercant marco = new Commercant("Marco", 20);
+        Ronin roro = new Ronin("Roro", "shochu", 60);
+
+        roro.direBonjour();
+        roro.donner(marco);
+        marco.boire(); // Pour voir le commerçant boire après avoir reçu de l'argent
+    }
+ 
+}
+
+
---
+++ src/personnages/Humain.java
@@ -0,0 +1,51 @@
+package personnages;
+
+public class Humain {
+    private String nom;
+    private String boissonFavorite;
+    private int argent;
+
+    public Humain(String nom, String boissonFavorite, int argent) {
+        this.nom = nom;
+        this.boissonFavorite = boissonFavorite;
+        this.argent = argent;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getArgent() {
+        return argent;
+    }
+
+    public void direBonjour() {
+        parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFavorite + ".");
+    }
+
+    public void boire() {
+        parler("Mmmm, un bon verre de " + boissonFavorite + " ! GLOUPS !");
+    }
+
+    public void acheter(String bien, int prix) {
+        if (argent >= prix) {
+            argent -= prix;
+            parler("J'ai " + argent + " sous en poche. Je viens de m'offrir un(e) " + bien + " à " + prix + " sous.");
+        } else {
+            parler("Je n'ai pas assez d'argent pour m'offrir un(e) " + bien + ".");
+        }
+    }
+
+    protected void gagnerArgent(int gain) {
+        argent += gain;
+    }
+
+    protected void perdreArgent(int perte) {
+        argent -= perte;
+    }
+
+    protected void parler(String texte) {
+        System.out.println("(" + nom + ") - " + texte);
+    }
+}
+
